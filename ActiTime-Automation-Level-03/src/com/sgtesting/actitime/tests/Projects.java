379ba6239e6ccc3fcedd90a9f50ca62e3ffc698a
machaniajay1015 <machaniajaykrishna@gmail.com>
2022-01-20 11:04:39 +0530

    ActiTime Automation Level 03

---
+++ ActiTime-Automation-Level-03/src/com/sgtesting/actitime/tests/Projects.java
@@ -0,0 +1,94 @@
+package com.sgtesting.actitime.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Projects {
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createProject(WebDriver oBrowser)
+	{
+		try
+		{
+			 oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[1]/div[2]/div")).click();
+	   		  Thread.sleep(2000);
+	   		  oBrowser.findElement(By.xpath("//div[text()='+ New Project']")).click();
+	   		  Thread.sleep(2000);
+	   		  oBrowser.findElement(By.id("projectPopup_projectNameField")).sendKeys("AUTOMATION");
+	   		  Thread.sleep(2000);
+	   		  oBrowser.findElement(By.xpath("//span[text()='Create Project']")).click();
+	   		  Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyProject(WebDriver oBrowser)
+	{
+		try
+		{
+
+			 oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[3]/div[3]")).click();
+   		  Thread.sleep(2000);
+   		  oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[4]/div[2]/div[1]/div[1]/div[1]/div/div[1]/div[3]/div/div[1]")).click();
+   		  Thread.sleep(2000);
+   		  oBrowser.findElement(By.xpath("/html/body/div[26]/div[2]/div")).click();
+   		  Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteProject(WebDriver oBrowser)
+	{
+		try
+		{
+			 oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[3]/div[3]")).click();
+	  		  Thread.sleep(2000);
+	  		  oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[4]/div[1]/div[2]/div[3]/div/div")).click();
+	  		  Thread.sleep(2000);
+	  		  oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[4]/div[4]/div/div[3]/div")).click();
+	  		  Thread.sleep(2000);
+	  		  oBrowser.findElement(By.xpath("//*[@id=\"projectPanel_deleteConfirm_submitTitle\"]")).click();
+	  		  Thread.sleep(2000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
