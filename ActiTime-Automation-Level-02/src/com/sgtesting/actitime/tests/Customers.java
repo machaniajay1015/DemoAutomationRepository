587f708bec2307598c7b77252becb8ebea7cf91f
machaniajay1015 <machaniajaykrishna@gmail.com>
2022-01-20 10:58:58 +0530

    ActiTime Automation Level02

---
+++ ActiTime-Automation-Level-02/src/com/sgtesting/actitime/tests/Customers.java
@@ -0,0 +1,93 @@
+package com.sgtesting.actitime.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Customers {
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createCustomer(WebDriver oBrowser)
+	{
+		try
+		{
+
+			oBrowser.findElement(By.xpath("//*[@id=\"topnav\"]/tbody/tr/td[3]/a")).click();
+			oBrowser.findElement(By.xpath("//div[text()='Add New']")).click();
+			oBrowser.findElement(By.xpath("//div[text()='+ New Customer']")).click();
+			oBrowser.findElement(By.id("customerLightBox_nameField")).sendKeys("ajaykrishna");
+			oBrowser.findElement(By.id("customerLightBox_descriptionField")).sendKeys("cicilenginer");
+			oBrowser.findElement(By.xpath("//*[@id=\"customerLightBox_commitBtn\"]/div/span")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyCustomer(WebDriver oBrowser)
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[2]/div[2]/div[1]/div[2]/div[1]/div[3]/div/div[1]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//div[text()='Archived']")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteCustomer(WebDriver oBrowser)
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\"taskListBlock\"]/div[2]/div[1]/div[4]/div/div")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='Delete']")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("customerPanel_deleteConfirm_submitTitle")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
