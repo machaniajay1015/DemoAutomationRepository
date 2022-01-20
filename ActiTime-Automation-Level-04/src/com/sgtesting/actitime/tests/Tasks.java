d9cf4e6f1d96add8b94ad4547f8540b563348db9
machaniajay1015 <machaniajaykrishna@gmail.com>
2022-01-20 14:46:40 +0530

    ActiTimeAutomation Level4

---
+++ ActiTime-Automation-Level-04/src/com/sgtesting/actitime/tests/Tasks.java
@@ -0,0 +1,73 @@
+package com.sgtesting.actitime.tests;
+
+import org.openqa.selenium.WebDriver;
+
+public class Tasks {
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createTask(WebDriver oBrowser)
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyTask(WebDriver oBrowser)
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteTask(WebDriver oBrowser)
+	{
+		try
+		{
+
+		}catch(Exception e)
+		{
+			e.printStackTrace();		
+		}
+	}
+
+}
